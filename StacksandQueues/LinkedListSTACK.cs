8306d1b40efa3046575459aff8e5f5174cb659fe
divatepratik10 <pratikrdivate3210@gmail.com>
2022-04-29 21:22:01 +0530

    [Pratik] added : UC1 creat stack programme

---
+++ StacksandQueues/LinkedListSTACK.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StacksandQueues
+{
+    internal class LinkedListSTACK
+    {
+        private Node top;
+
+        public LinkedListSTACK()
+        {
+            this.top = null;
+        }
+
+        //PUSH OPERATION
+        public void Push(int Val)
+        {
+            Node node = new Node(Val);
+            if(this.top == null)
+            {
+                node.next = null;
+            }
+            else
+            {
+                node.next = this.top;
+            }
+            this.top = node;
+
+            Console.WriteLine("{0} is Pushed into the STACK.", Val);
+        }
+
+        public void Display()
+        {
+            Node temp = this.top;
+            while(temp != null)
+            {
+                Console.WriteLine("Stack Data : " +temp.data);
+                temp = temp.next;
+            }
+        }
+    }
+}
---
+++ StacksandQueues/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StacksandQueues
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("WELCOME TO LINKED-LIST STACK PROGRAMME\n");
+            LinkedListSTACK linkedListSTACK = new LinkedListSTACK();
+            linkedListSTACK.Push(70);
+            linkedListSTACK.Push(30);
+            linkedListSTACK.Push(56);
+            Console.WriteLine("\nFinal Stack list is : ");
+            linkedListSTACK.Display();
+
+            Console.ReadLine();
+        }
+    }
+}
